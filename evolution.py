b81f3301fe11c48bd18a9f32188cc2c9004715da
ChristophHoenes <christoph.hoenes@gmail.com>
2020-10-08 19:32:07 +0200

    folder name list of enemies

--- evolution.py
+++ evolution.py
@@ -47,7 +47,7 @@ def start_evolution(args, config):
         toolbox.register("map", map)
 
     # create directory for experiment results
-    save_dir = "{}_enemy{}_seed_{}".format(args.config.split(".json")[0], args.enemies[0], args.seed)
+    save_dir = "{}_enemy{}_seed_{}".format(args.config.split(".json")[0], args.enemies, args.seed)
     save_path = os.path.join(os.getcwd(), save_dir)
     if not os.path.exists(save_path):
         os.mkdir(save_path)
